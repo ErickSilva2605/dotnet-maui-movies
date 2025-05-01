d2be3d95c15de485ea12a9e22908ba070761763e
ErickSilva2605 <erick.dev.it@gmail.com>
2025-05-01 19:42:45 -0300

    feat: setup dos testes de ui com appium

--- src/MauiMovies.UI/Platforms/Android/MainActivity.cs
+++ src/MauiMovies.UI/Platforms/Android/MainActivity.cs
@@ -1,11 +1,12 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
 
-namespace MauiMovies.UI
+namespace MauiMovies.UI;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Register("com.mauimovies.MainActivity")]
+public class MainActivity : MauiAppCompatActivity
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public class MainActivity : MauiAppCompatActivity
-    {
-    }
 }
---
+++ tests/MauiMovies.UITests.Shared/UITest1.cs
@@ -0,0 +1,35 @@
+using Xunit;
+
+// You will have to make sure that all the namespaces match
+// between the different platform specific projects and the shared
+// code files. This has to do with how we initialize the AppiumDriver
+// through the AppiumSetup.cs files and NUnit SetUpFixture attributes.
+// Also see: https://docs.nunit.org/articles/nunit/writing-tests/attributes/setupfixture.html
+namespace MauiMovies.UITests;
+
+// This is an example of tests that do not need anything platform specific.
+// Typically you will want all your tests to be in the shared project so they are ran across all platforms.
+public class UITest1 : BaseTest
+{
+    [Fact]
+    public void AppLaunches()
+    {
+        App.GetScreenshot().SaveAsFile($"{nameof(AppLaunches)}.png");
+    }
+
+    [Fact]
+    public async Task ClickCounterTest()
+    {
+        // Arrange
+        // Find elements with the value of the AutomationId property
+        var element = FindUIElement("CounterBtn");
+
+        // Act
+        element.Click();
+        await Task.Delay(500); // Wait for the click to register and show up on the screenshot
+
+        // Assert
+        App.GetScreenshot().SaveAsFile($"{nameof(ClickCounterTest)}.png");
+        Assert.Equal("Clicked 1 time", element.Text);
+    }
+}
