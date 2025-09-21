df361888003d01f82547543cead68075c5e71173
ErickSilva2605 <erick.dev.it@gmail.com>
2025-09-21 18:29:05 -0300

    feat: adicionando dto de erro

---
+++ src/MauiMovies.Infrastructure/Api/Dtos/ErrorResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MauiMovies.Infrastructure.Api.Dtos;
+
+public class ErrorResponseDto
+{
+	[JsonPropertyName("success")]
+	public bool Success { get; set; }
+
+	[JsonPropertyName("status_code")]
+	public int StatusCode { get; set; }
+
+	[JsonPropertyName("status_message")]
+	public string? StatusMessage { get; set; }
+}
