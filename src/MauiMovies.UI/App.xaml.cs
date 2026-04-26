d53317808f81a6ba16fbaddf11a96edf09c98e70
ErickSilva2605 <erick.dev.it@gmail.com>
2026-04-26 19:15:56 -0300

    feat: sync status bar color with app theme on Android and iOS

--- src/MauiMovies.UI/App.xaml.cs
+++ src/MauiMovies.UI/App.xaml.cs
@@ -1,14 +1,39 @@
-﻿namespace MauiMovies.UI;
+﻿#if ANDROID || IOS
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Core.Platform;
+#endif
+
+namespace MauiMovies.UI;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
+		RequestedThemeChanged += (_, e) => ApplyStatusBar(e.RequestedTheme);
 	}
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
 		return new Window(new AppShell());
+	}
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+		ApplyStatusBar(RequestedTheme);
+	}
+
+	static void ApplyStatusBar(AppTheme theme)
+	{
+#if ANDROID
+		if (!OperatingSystem.IsAndroidVersionAtLeast(23))
+			return;
+#endif
+#if ANDROID || IOS
+		var isDark = theme == AppTheme.Dark;
+		StatusBar.SetColor(isDark ? Color.FromArgb("#031D33") : Colors.White);
+		StatusBar.SetStyle(isDark ? StatusBarStyle.LightContent : StatusBarStyle.DarkContent);
+#endif
 	}
 }
