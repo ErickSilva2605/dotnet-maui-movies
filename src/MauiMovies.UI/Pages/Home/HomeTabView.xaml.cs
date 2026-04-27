f4e24bda60ee8a53bde8d68d506963423582a0fb
ErickSilva2605 <erick.dev.it@gmail.com>
2026-04-27 12:44:03 -0300

    feat(ui): redesign Home with Trending backdrop, Day/Week segmented control and consolidate card styles

--- src/MauiMovies.UI/Pages/Home/HomeTabView.xaml.cs
+++ src/MauiMovies.UI/Pages/Home/HomeTabView.xaml.cs
@@ -1,4 +1,3 @@
-using MauiMovies.Core.Models;
 using MauiMovies.Core.ViewModels;
 
 namespace MauiMovies.UI.Pages.Home;
@@ -9,16 +8,5 @@ public partial class HomeTabView : ContentView
 	{
 		BindingContext = viewModel;
 		InitializeComponent();
-	}
-
-	async void OnTrendingSelectionChanged(object? sender, SelectionChangedEventArgs e)
-	{
-		if (sender is not CollectionView collectionView || BindingContext is not HomeViewModel viewModel)
-			return;
-
-		if (e.CurrentSelection.FirstOrDefault() is MediaItemModel item)
-			await viewModel.NavigateToMediaItemCommand.ExecuteAsync(item);
-
-		collectionView.SelectedItem = null;
 	}
 }
